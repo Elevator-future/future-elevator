3370c23d271940ec235eff73b9853e35665ff73a
Kapkaev Andrey <kapkaew@yandex.ru>
2023-01-15 09:31:34 +0300

    Add Lesson 4

---
+++ М1 Урок 4 Вложенные структуры данных/uzon_db.py
@@ -0,0 +1,69 @@
+database = {
+    'count': 5,
+    'items': [
+        {
+            'category': 'электроника',
+            'name': 'Ryzen 5 5900X',
+            'quantity': 0,
+            'info': {
+                'rating': 4.5,
+                'price': 100000,
+                'description': 'AMD Ryzen 5 5900X 3.7GHz 12-Core AM4 Boxed Processor'
+            },
+            "characteristics": {
+                "clock_speed": 3.6,
+                "cores": 12,
+            }
+        },
+        {
+            'category': 'электроника',
+            'name': 'Ryzen 5 5600X',
+            'quantity': 0,
+            'info': {
+                'rating': 4.7,
+                'price': 80000,
+                'description': 'AMD Ryzen 5 5600X 3.7GHz 6-Core AM4 Boxed Processor'
+            },
+            "characteristics": {
+                "clock_speed": 3.8,
+                "cores": 6,
+            }
+        },
+        {
+            'category': 'смартфон',
+            'name': 'iPhone 12 Pro',
+            'quantity': 2,
+            'info': {
+                'rating': 4.9,
+                'price': 150000,
+                'description': 'Apple iPhone 12 Pro 128GB 6.1" Display 5G Smartphone'
+            },
+            "characteristics": {
+                "weight": 0.2
+            }
+        },
+        {
+            'category': 'бытовая техника',
+            'name': 'фен PHILIPS',
+            'quantity': 1,
+            'info': {
+                'rating': 4.8,
+                'price': 5000,
+                'description': 'фен PHILIPS HP8230/00'
+            },
+            "characteristics": {
+                "weight": 0.8
+            }
+        },
+        {
+            'category': 'чай',
+            'name': 'Молочный улун',
+            'quantity': 30,
+            'info': {
+                'rating': 4.6,
+                'price': 1000,
+                'description': 'чай Молочный улун'
+            }
+        }
+    ]
+}
