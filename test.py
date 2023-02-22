dcbfbdbdb9b6004138fb2d03cbc430bd0d10b2f1
Kapkaev Andrey <kapkaew@yandex.ru>
2023-02-22 23:03:50 +0300

    Added Lesson 13

--- test.py
+++ test.py
@@ -1,6 +1,3 @@
-m = int(input('Pow number >>>'))
-
-
-for i in range(1, 11):
-    print(f"{m} x {i} = {m * i}")
-
+print(
+    *[num ** 2 for num in list(map(int, input('My numbers >>>').split())) if num % 2 == 0 and num % 10 != 4 and num != 2]
+)
